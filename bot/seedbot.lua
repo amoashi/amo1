592bda7bd551504709231612e40f9e45475b3ad4
imashiam <ashkanpj.ap2@gmail.com>
2016-03-10 14:01:49 +0330

    A H D

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,7 +228,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {81444630,49345746},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[.::::A H D::::. v4
 An advanced administration bot based on TG-CLI written in Lua
